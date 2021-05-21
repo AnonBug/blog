08104fef132fe416d18b174a9e152fb565732e8b
AnonBug <1248758606@qq.com>
2021-05-21 18:58:03 +0800

    同步实现异步代码

--- gits.js
+++ gits.js
@@ -4,18 +4,34 @@ const ADD = 'git add .'
 const COMMIT = `git commit -m ${process.argv[2]}`
 const PUSH = 'git push github main'
 
-exec(ADD, (err) => {
-    if (err) console.log(err);
-    exec(COMMIT, (err) => {
-        if (err) console.log(err);
-        exec(PUSH, (err, stdout, stderr) => {
-            if (err) console.log(err);
-
-            console.log(stdout);
-            exec('./deploy.sh', (err, stdout, stderr) => {
-                if (err) console.log(err);
-                console.log(stdout);
-            })
+/* 同步实现异步代码 */
+const execAsync = cmd => {
+    return new Promise((resolve, reject) => {
+        exec(cmd, (err, stdout, stderr) => {
+            if (err) reject(err)
+            resolve(stdout)
         })
     })
-})
+}
+
+(async () => {
+    await execAsync(ADD)
+    await execAsync(COMMIT)
+    await execAsync(PUSH)
+})()
+
+// exec(ADD, (err) => {
+//     if (err) console.log(err);
+//     exec(COMMIT, (err) => {
+//         if (err) console.log(err);
+//         exec(PUSH, (err, stdout, stderr) => {
+//             if (err) console.log(err);
+
+//             console.log(stdout);
+//             exec('./deploy.sh', (err, stdout, stderr) => {
+//                 if (err) console.log(err);
+//                 console.log(stdout);
+//             })
+//         })
+//     })
+// })
