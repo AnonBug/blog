b6efe2723e7048bed32d2b92d5c4348ffd095d20
AnonBug <1248758606@qq.com>
2021-05-22 18:25:22 +0800

    增加摸鱼主题

--- gits.js
+++ gits.js
@@ -4,21 +4,11 @@ const commitMsg = process.argv[2]
 
 const cmds = [
     'git add .', `git commit -m ${commitMsg}`, 'git push github main',
-    // 'cd ./codes',
+    'cd ./codes',
     // 'git init', 'git add -A', `git commit -m ${commitMsg}`,
     // 'git push -f git@github.com:AnonBug/blog.git master:gh-pages',
     // 'cd -'
 ]
-
-/* 同步实现异步代码 */
-const execAsync = cmd => {
-    return new Promise((resolve, reject) => {
-        exec(cmd, (err, stdout, stderr) => {
-            if (err) reject(err)
-            resolve(stdout)
-        })
-    })
-}
 
 try {
     for (let cmd of cmds) {
@@ -28,19 +18,3 @@ try {
 } catch (e) {
     console.log(e);
 }
-
-// exec(ADD, (err) => {
-//     if (err) console.log(err);
-//     exec(COMMIT, (err) => {
-//         if (err) console.log(err);
-//         exec(PUSH, (err, stdout, stderr) => {
-//             if (err) console.log(err);
-
-//             console.log(stdout);
-//             exec('./deploy.sh', (err, stdout, stderr) => {
-//                 if (err) console.log(err);
-//                 console.log(stdout);
-//             })
-//         })
-//     })
-// })
